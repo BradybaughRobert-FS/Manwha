2ce72f07b72369ec41982ccbd48e4f7a298db45a
bradybaughrobert-fs <rlbradybaugh@student.fullsail.edu>
2024-08-27 16:15:43 -0400

    updated folders

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-const app = require("./app");
-const connectDB = require("./app/db/config")
+require("dotenv").config(); // Ensure this is at the very top to load environment variables first
+const app = require("./app"); // Assuming 'app' is your Express application module
+const connectDB = require("./app/db/config"); // Database connection setup
 
 connectDB();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Use PORT from environment variables if available, otherwise default to 3000
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 });
